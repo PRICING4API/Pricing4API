43f5606db720195aa3b30fba08b9987db1f34fef
danielruizlopezcc <danielruizlopezcc@gmail.com>
2024-02-06 18:23:04 +0100

    Implemented 'compute_t_ast' function and added test

--- Pricing4API/src/plan.py
+++ Pricing4API/src/plan.py
@@ -81,8 +81,8 @@ class Plan:
         self.__previous_plan = self
 
         self.__m=len(self.__q)-1
-        # self.__t_ast=self.compute_t_ast(self.__limits)
-
+        self.__t_ast=self.compute_t_ast(self.__limits)
+        #self.__t_ast=self.compute_t_ast()
         
 
 
@@ -293,6 +293,16 @@ class Plan:
 
         return T
     
+    def compute_t_ast(self, limits: List[Tuple[int, int]])-> List[int]:
+        t_ast = [0] + [None for _ in range(1, len(limits)-1)]
+
+        for i in range(1,len(limits)-1):
+            t_ast[i] = self.min_time(limits[i][0], limits, i_initial=i-1)
+
+        return t_ast
+    
+    
+    
 
     ## Auxiliary functions
     def adjust_time_unitsx(self, t_max:int) -> (str,int):
@@ -311,215 +321,7 @@ class Plan:
             scale = 86400
         return units, scale
     
-    def show_rate_curve(self, time_interval:int) -> None:
-        """ shows the available requests in the plan over time"""
-        units, scale =  self.adjust_time_unitsx(time_interval)
-        # dado que el rate es una función escalón, no tiene sentido obtener más valores que la unida del rate t`0
-        time_values = np.arange(0, time_interval, self.rate_frequency)  # Define el rango de valores de c que desees
-
-        # Calcula los valores de capacidad del rate a representar
-        capacity = self.rate_function(time_values)
-        # Crea el gráfico
-        
-        print(time_values)
-        print(capacity)
-        # Crear la figura principal con dos subgráficos
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-
-        # Gráfico principal (la curva completa)
-        ax1.step(time_values, capacity, where='post')
-        ax1.fill_between(time_values[:-1], capacity[:-1], step='post', color='green', alpha=0.3)  # Rellenar el área bajo la curva
-        ax1.set_ylim(0, max(capacity) + 1)  # Forzar el eje Y para que comience en 0
-        ax1.set_xlabel(f'Time ({units})')
-        ax1.set_ylabel('Requests')
-        ax1.set_title(f'Rate curve of plan {self.name}. ({self.rate_value}  req / {self.rate_frequency} sec)')
-        ax1.grid(True)  # Agregar un grid
-
-        # Calcular la intersección con la función en x=60
-        x_value = 60
-        y_value = self.rate_function(x_value)
-
-        # Dibujar la línea roja que termina en la intersección con la función
-        ax1.plot([x_value, x_value], [0, y_value], color='red', linestyle='--')
-        ax1.plot([0, x_value], [y_value, y_value], color='red', linestyle='--')
-
-
-        # Mostrar los valores de x e y para la línea roja sin que intersecten con los ejes
-        ax1.text(x_value, -0.2, f'x={x_value}', fontsize=12, ha='center')
-        ax1.text(-1, y_value + 0.2, f'y={y_value}', fontsize=12, va='center')
-
-        # Subgráfico (solo los primeros 20 segundos)
-        units, scale =  self.adjust_time_unitsx(10)
-        ax2.step(time_values[:11], capacity[:11], where='post')
-        ax2.fill_between(time_values[:11], capacity[:11], step='post', color='green', alpha=0.3)  # Rellenar el área bajo la curva
-        # Dibujar círculos sólidos en cada punto del gráfico
-        ax2.plot(time_values[:11], capacity[:11], 'o', markersize=3, color='green')
-
-        ax2.set_ylim(0, max(capacity[:11]) + 1)  # Forzar el eje Y para que comience en 0
-        ax2.set_xlabel(f'Time ({units})')
-        ax2.set_ylabel('Requests')
-        ax2.set_title(f'First {10*self.__t[0]} seconds') 
-        # Establecer las ubicaciones del grid en el eje x
-        ax2.set_xticks(time_values[:11])
-        ax2.grid(True)  
-
-        # Mostrar la figura con los dos subgráficos
-        plt.tight_layout()
-        plt.show()
     
-    
-    
-    # def show_quote_curve(self, time_interval:int) -> None:
-    #     """ shows the available requests in the plan over time"""
-    #     units, scale =  self.adjust_time_unitsx(time_interval)
-        
-    #     # dado que el rate es una función escalón, no tiene sentido obtener más valores que la unida del rate t`0
-    #     if len(self.quote_frequency)>0:
-    #         time_values = np.arange(0, time_interval, self.quote_frequency[0])
-    #     else:
-    #         time_values = None
-
-    #     if time_values is not None:
-    #         # Calculate the values of capacity of the rate to be represented
-    #         capacity = self.quote_function(time_values)
-    #         # Create the plot
-    #         print(f"This are the time values:{time_values}")
-    #         print(f"This are the capacity values:,{capacity}")
-    #         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-
-    #         # Gráfico principal (la curva completa)
-    #         ax1.step(time_values, capacity, where='post')
-    #         ax1.fill_between(time_values[:-1], capacity[:-1], step='post', color='green', alpha=0.3)  # Rellenar el tamaño bajo la curva
-    #         ax1.set_ylim(0, max(capacity) + 1)  #
-    #         ax1.set_xlabel(f'Time ({units})')
-    #         ax1.set_ylabel('Requests')
-    #         ax1.set_title(f'Quote curve of plan {self.name}. ({self.quote_value[0]}  req / {self.quote_frequency[0]} sec)')
-    #         ax1.grid(True)  # Agregar un grid
-
-    #         # Calcular la intersección con la función en x=60
-    #         x_value = 60
-    #         y_value = self.quote_function(x_value)
-
-    #         # Dibujar la línea roja que termina en la intersección con la función
-    #         ax1.axvline(x=x_value, ymin=0, ymax=y_value/max(capacity), color='red', linestyle='--')
-    #         ax1.axhline(y=y_value, xmin=0, xmax=x_value/max(time_values), color='red', linestyle='--')
-
-    #         # Mostrar los valores de x e y para la línea roja sin que intersecten con los ejes
-    #         ax1.text(x_value, -0.2, f'x={x_value}', fontsize=12, ha='center')
-    #         ax1.text(-1, y_value + 0.2, f'y={y_value}', fontsize=12, va='center')
-
-    #         # Subgráfico (solo los primeros 20 segundos)
-    #         units, scale =  self.adjust_time_unitsx(time_interval)
-    #         ax2.step(time_values[:11], capacity[:11], where='post')
-    #         ax2.fill_between(time_values[:11], capacity[:11], step='post', color='green', alpha=0.3)  # Rellenar elSetBranch bajo la curva
-    #         # Dibujar círculos.masks en cada punto del gráfico
-    #         ax2.plot(time_values[:11], capacity[:11], 'o', markersize=3, color='green')
-
-    #         ax2.set_ylim(0, max(capacity[:11]) + 1)  # Forzar el eje Y para que comience en 0
-    #         ax2.set_xlabel(f'Time ({units})')
-    #         ax2.set_ylabel('Requests')
-    #         ax2.set_title(f'First {10*self.__t[0]} seconds')
-    #         # Establecer las ubicaciones del grid en el eje x
-    #         ax2.set_xticks(time_values[:11])
-    #         ax2.grid(True)
-
-    #         # Mostrar la figura con los dos subgráficos
-    #         plt.tight_layout()
-    #         plt.show()
-            
-    #     else:
-    #         print("time_values is None")
-        
-       
-
-    def show_quote_curve(self, time_interval: int = None) -> None:
-        """ shows the available requests in the plan over time"""
-        if time_interval is None:
-            time_interval = max(self.quote_frequency)
-            
-        units, scale =  self.adjust_time_unitsx(time_interval)
-        
-        if len(self.quote_frequency) > 0:
-            fig, ax = plt.subplots(figsize=(8, 8))
-            patches = []
-            colors = [(0, 1, 0), (0, 0, 1)]  # Define the colors in RGB
-
-            for i, (quote_value, quote_frequency) in enumerate(zip(self.quote_value, self.quote_frequency)):
-                time_values = np.arange(0, time_interval + 1, quote_frequency)
-                capacity = quote_value + quote_value * (time_values // quote_frequency)
-
-                print(f"This are the time values:{time_values}")
-                print(f"This are the capacity values:,{capacity}")
-
-                color= colors[i]  # Generate a random color
-                ax.step(time_values, capacity, where='post')
-                ax.fill_between(time_values, capacity, step='post', color=color, alpha=0.1)
-
-                patch = mpatches.Patch(color=color, label=f'{quote_value} req / {quote_frequency} sec')
-                patches.append(patch)
-
-                ax.set_ylim(0, max(capacity) + 1)
-                ax.set_xlabel(f'Time ({units})')
-                ax.set_ylabel('Requests')
-                ax.set_title(f'Quote curve of plan {self.name}.')
-                ax.grid(True)
-
-            ax.legend(handles=patches)
-            plt.tight_layout()
-            plt.show()
-        else:
-            print("The plan has no quotes.")
-
-
-    
-            
-            
-
-    
-    
-    
-
-
-    # def maximum_disruption_period(self) -> int:
-    #     """
-    #     Calculates the maximum disruption period based on the quote, rate, quote_unit, and rate_unit variables.
-
-    #     :return: The maximum disruption period.
-    #     :rtype: int
-    #     """
-    #     assert self.__quote is not None and self.__rate is not None and self.__quote_unit is not None and self.__rate_unit is not None, "Variables quote, rate, quote_unit, and rate_unit must be defined before maximum_disruption_period is called"
-
-    #     tt = (self.__quote / self.__rate) * self.__rate_unit
-    #     tq = self.__quote_unit
-    #     return tq - tt
-
-    # def max_time_to_consume_at(self, rate: int) -> int:
-        
-    #     tt =(self.__quote / rate) * self.__rate_unit
-
-    #     return tt
-    
-
-    # def maximum_quote(self) -> float:
-    #     return self.__quote * self.__max_number_of_subscriptions
-    
-    # def maximum_rate(self) -> float:
-    #     return self.__rate * self.__max_number_of_subscriptions
-
-    # def cost(self, time: int, requests: int) -> float:
-    #     C_0=self.__price
-    #     C_1=self.__overage_cost
-    #     plan_capacity = self.capacity(time)
-
-    #     if requests > plan_capacity:
-    #         return -1
-    #     return C_0 * (math.ceil(time/self.__billing_unit)) + heaviside(requests-self.capacity(time))*C_1*(requests-self.capacity(time))
-
-    # def cost_effective_threshold(self, plan)-> int:
-    #     assert self.__price <= plan.price, "The price of plan " + plan.name + " must be greater than plan " + self.__name
-
-    #     threshold = self.__quote + math.floor((plan.price - self.__price)/self.__overage_cost)
-    #     return threshold
 
 
 
--- Pricing4API/validationTest.py
+++ Pricing4API/validationTest.py
@@ -2,7 +2,7 @@
 from typing import List, Tuple
 from src.plan import Plan
 from src.pricing import Pricing
-from src.utils import format_time
+
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -23,7 +23,9 @@ PricingDBLP = Pricing('DBLP', LDBLP, 'queries')
 
 
 
+def error_msg(msg)->str:
 
+    return("\033[31m!" + msg + "\033[0m")
 
 def pass_msg(msg:str)->str:
 
@@ -62,6 +64,54 @@ def test_min_time(limits: List[Tuple[int, int]],list_c_t: List[Tuple[int, int]],
         print(f"\033[31m{e}\033[0m")
 
 
+t_ast = PlanProDBLP.compute_t_ast(PlanProDBLP.limits)
+# t_ast = PlanProDBLP.compute_t_ast()
+
+def test_min_time_automated(limits: List[Tuple[int, int]], name:str="" ):
+
+    print("Testing min_time by using accumulated capacity! " + name)
+
+    try:
+
+        # Test the min_time function comparing it with the recursive version of capacity
+
+        inconsistency_found = False
+
+        num_tests = 12000
+
+        t_values = range(0, num_tests)
+
+        for time in t_values:
+
+            accum_capacity  = PlanProDBLP.accumulated_capacity(time, len(limits) - 1, limits)
+
+            m_time = PlanProDBLP.min_time(accum_capacity,limits)
+
+            #dado con min_time te devuelve el suelo del tiempo, hay que ser cuidadoso al comparar
+
+            # los instnataneos de tiempo que no sean múltiplos del rate hay que pasarlos al múltoplo próximo más cercano por debajo
+
+            t_aux= (time//limits[0][1])*limits[0][1]
+
+            #también hay que ser cuidados con la capacidad entre múltiplos de t* + la quota
+
+            if (m_time % (t_ast[1]!=0 + limits[0][1]) and t_aux != m_time): #si el tiempo mínimo es múltiplo de t* y no es múltiplo de rate
+
+                inconsistency_found = True
+
+                message=f"Inconsistency found: accum_capacity(t={int(time)})={int(accum_capacity)}, min_time(C={int(accum_capacity)})={int(m_time)}(taux={int(t_aux)}"
+
+                print(error_msg(message))
+
+        if inconsistency_found == False:
+
+            print(pass_msg(f"Min-time function passed the {num_tests} on {name} automatically generated!"))
+
+    except AssertionError as e:
+
+        print(f"\033[31m{e}\033[0m")
+
+
 list_t_c = [(0, 1), (1, 1), (2, 2), (3, 2), (4, 3), (5, 3), (38, 20), (39, 20), (40, 20), (41, 20), (60,21), (98, 40), (99, 40)]
 list_c_t = [(0, 0), (1, 0), (2, 2), (3, 4), (4, 6), (5, 8), (20, 38), (21, 60)]
 
@@ -69,6 +119,7 @@ list_c_t = [(0, 0), (1, 0), (2, 2), (3, 4), (4, 6), (5, 8), (20, 38), (21, 60)]
 if __name__ == '__main__':
     test_capacity(PlanProDBLP.limits, list_t_c, "PlanProDBLP")
     test_min_time(PlanProDBLP.limits, list_c_t, "PlanProDBLP")
+    test_min_time_automated(PlanProDBLP.limits, "PlanProDBLP")
     
     print(PlanProDBLP.limits)
 
