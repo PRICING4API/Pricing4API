f9c508301314a0c31ac1f40b1aa0d055e96172c1
JaviGarc1a <javgaragu1@alum.us.es>
2024-07-17 19:36:46 +0200

    refactor: Improve test script

--- Pricing4API/test_plan_sla4oai.py
+++ Pricing4API/test_plan_sla4oai.py
@@ -14,7 +14,7 @@ def read_amadeus_yaml(yaml_file):
             print(exc)
 
 
-yaml_file = "Pricing4API/amadeus.yaml"
+yaml_file = "Pricing4API/sla4oai-models/sendgrid-sla4oai.yaml"
 
 data = read_amadeus_yaml(yaml_file)
 
@@ -25,121 +25,65 @@ billing = sla.get_billing()
 
 plans_name = sla.get_plans().get_names_of_plans()
 
-urls_rate_test = sla.get_plans().get_plan_by_name(plans_name[0]).get_rates().get_paths()
+for plan_name in plans_name:
 
-method_rate_test = (
-    sla.get_plans()
-    .get_plan_by_name(plans_name[0])
-    .get_rates()
-    .get_methods_by_path(urls_rate_test[0])
-)
+    urls_rate_test = sla.get_plans().get_plan_by_name(plan_name).get_rates().get_paths()
 
-request_rate_test = (
-    sla.get_plans()
-    .get_plan_by_name(plans_name[0])
-    .get_rates()
-    .get_limit_by_method(urls_rate_test[0], method_rate_test[0])[0]
-    .get_max()
-)
+    rate_test = None
 
-time_rate_test = time_unit_to_seconds(
-    sla.get_plans()
-    .get_plan_by_name(plans_name[0])
-    .get_rates()
-    .get_limit_by_method(urls_rate_test[0], method_rate_test[0])[0]
-    .get_period()
-    .get_unit()
-)
-
-rate_test = (request_rate_test, time_rate_test)
-
-urls_quotas_test = (
-    sla.get_plans().get_plan_by_name(plans_name[0]).get_quotas().get_paths()
-)
-
-quotas_test = [
-    (
-        sla.get_plans()
-        .get_plan_by_name(plans_name[0])
-        .get_quotas()
-        .get_max_by_path_and_method(url, method),
-        time_unit_to_seconds(
+    if urls_rate_test:
+        method_rate_test = (
             sla.get_plans()
-            .get_plan_by_name(plans_name[0])
-            .get_quotas()
-            .get_period_by_path_and_method(url, method)
-        ),
-    )
-    for url in urls_quotas_test
-    for method in sla.get_plans()
-    .get_plan_by_name(plans_name[0])
-    .get_quotas()
-    .get_methods_by_path(url)
-]
+            .get_plan_by_name(plan_name)
+            .get_rates()
+            .get_methods_by_path(urls_rate_test[0])
+        )
 
-plan_test = Plan(f"Amadeus-{plans_name[0]}", billing, rate_test, quotas_test)
-
-urls_rate_production = (
-    sla.get_plans().get_plan_by_name(plans_name[1]).get_rates().get_paths()
-)
-
-method_rate_production = (
-    sla.get_plans()
-    .get_plan_by_name(plans_name[1])
-    .get_rates()
-    .get_methods_by_path(urls_rate_production[0])
-)
-
-request_rate_production = (
-    sla.get_plans()
-    .get_plan_by_name(plans_name[1])
-    .get_rates()
-    .get_limit_by_method(urls_rate_production[0], method_rate_production[0])[0]
-    .get_max()
-)
-
-time_rate_production = time_unit_to_seconds(
-    sla.get_plans()
-    .get_plan_by_name(plans_name[1])
-    .get_rates()
-    .get_limit_by_method(urls_rate_production[0], method_rate_production[0])[0]
-    .get_period()
-    .get_unit()
-)
-
-rate_production = (request_rate_production, time_rate_production)
-
-urls_quotas_production = (
-    sla.get_plans().get_plan_by_name(plans_name[1]).get_quotas().get_paths()
-)
-
-quotas_production = [
-    (
-        sla.get_plans()
-        .get_plan_by_name(plans_name[1])
-        .get_quotas()
-        .get_max_by_path_and_method(url, method),
-        time_unit_to_seconds(
+        request_rate_test = (
             sla.get_plans()
-            .get_plan_by_name(plans_name[1])
-            .get_quotas()
-            .get_period_by_path_and_method(url, method)
+            .get_plan_by_name(plan_name)
+            .get_rates()
+            .get_limit_by_method(urls_rate_test[0], method_rate_test[0])[0]
+            .get_max()
+        )
+
+        time_rate_test = time_unit_to_seconds(
+            sla.get_plans()
+            .get_plan_by_name(plan_name)
+            .get_rates()
+            .get_limit_by_method(urls_rate_test[0], method_rate_test[0])[0]
+            .get_period()
             .get_unit()
-        ),
+        )
+
+        rate_test = (request_rate_test, time_rate_test)
+
+    urls_quotas_test = (
+        sla.get_plans().get_plan_by_name(plan_name).get_quotas().get_paths()
     )
-    for url in urls_quotas_production
-    for method in sla.get_plans()
-    .get_plan_by_name(plans_name[1])
-    .get_quotas()
-    .get_methods_by_path(url)
-]
 
-plan_production = Plan(
-    f"Amadeus-{plans_name[1]}", billing, rate_production, quotas_production
-)
+    quotas_test = [
+        (
+            sla.get_plans()
+            .get_plan_by_name(plan_name)
+            .get_quotas()
+            .get_max_by_path_and_method(url, method),
+            time_unit_to_seconds(
+                sla.get_plans()
+                .get_plan_by_name(plan_name)
+                .get_quotas()
+                .get_period_by_path_and_method(url, method)
+            ),
+        )
+        for url in urls_quotas_test
+        for method in sla.get_plans()
+        .get_plan_by_name(plan_name)
+        .get_quotas()
+        .get_methods_by_path(url)
+    ]
 
-plans.append(plan_test)
-plans.append(plan_production)
+    plan_test = Plan(plan_name, billing, rate_test, quotas_test)
+    plans.append(plan_test)
 
 pricing = Pricing("Amadeus", plans, "requests")
 pricing.link_plans()
