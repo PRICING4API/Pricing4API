028e69460b74d83cbbec4b8f83d0437ecaa6bdfb
Ramón Gavira <ramgavsan@alum.us.es>
2025-04-09 17:04:47 +0200

    feat: compare bounded_rates

--- Pricing4API/basic/compare_curves.py
+++ Pricing4API/basic/compare_curves.py
@@ -58,3 +58,59 @@ def compare_rates_capacity(rates: List[Rate], time_interval: Union[str, TimeDura
         return fig
 
     fig.show()
+
+
+def compare_bounded_rates_capacity(bounded_rates: List[BoundedRate], time_interval: Union[str, TimeDuration], return_fig=False):
+    """
+    Compares the capacity curves of a list of bounded rates.
+
+    Args:
+        bounded_rates (List[BoundedRate]): List of bounded rates to compare.
+        time_interval (Union[str, TimeDuration]): The time interval for generating the curves.
+        return_fig (bool, optional): Whether to return the figure. Defaults to False.
+    """
+    if isinstance(time_interval, str):
+        time_interval = parse_time_string_to_duration(time_interval)
+
+    predefined_colors = [
+        "green", "purple", "brown", "pink", "gray", "olive", "cyan", "magenta", "teal", "lime"
+    ]
+
+    if len(bounded_rates) > len(predefined_colors):
+        raise ValueError("Not enough colors available for all bounded rates.")
+
+    fig = go.Figure()
+
+    for bounded_rate, color in zip(bounded_rates, predefined_colors):
+        debug_values = bounded_rate.show_available_capacity_curve(time_interval, debug=True)
+        times_ms, capacities = zip(*debug_values)
+
+        original_times = [t / time_interval.unit.to_milliseconds() for t in times_ms]
+
+        rgba_color = f"rgba({','.join(map(str, [int(c * 255) for c in to_rgba(color)[:3]]))},0.2)"
+
+        fig.add_trace(go.Scatter(
+            x=original_times,
+            y=capacities,
+            mode='lines',
+            line=dict(color=color, shape='hv', width=1.3),
+            fill='tonexty',
+            fillcolor=rgba_color,
+            name=f"Bounded Rate ({color}) with {bounded_rate.rate} rate"
+        ))
+
+    fig.update_layout(
+        title="Comparison of Bounded Rate Capacity Curves",
+        xaxis_title=f"Time ({time_interval.unit.value})",
+        yaxis_title="Capacity",
+        legend_title="Bounded Rates",
+        template="plotly_white",
+        width=1000,
+        height=600
+    )
+
+    if return_fig:
+        return fig
+
+    fig.show()
+            
